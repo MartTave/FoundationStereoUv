498d725cd32c5f90628714ea5bfe085f9a9aa757
MartTave <55973969+MartTave@users.noreply.github.com>
2025-06-18 14:01:42 +0200

    test_video

---
+++ scripts/run_video.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+
+import os
+import sys
+
+code_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(f"{code_dir}/../")
+from omegaconf import OmegaConf
+
+from core.foundation_stereo import *
+from core.utils.utils import InputPadder
+from Utils import *
+
+if __name__ == "__main__":
+    code_dir = os.path.dirname(os.path.realpath(__file__))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video", default=f"{code_dir}/../assets/left.png", type=str)
+    parser.add_argument("--intrinsic_file", default=f"{code_dir}/../assets/K.txt", type=str, help="camera intrinsic matrix and baseline file")
+    parser.add_argument("--ckpt_dir", default=f"{code_dir}/../pretrained_models/23-51-11/model_best_bp2.pth", type=str, help="pretrained model path")
+    parser.add_argument("--out_dir", default=f"{code_dir}/../output/", type=str, help="the directory to save results")
+    parser.add_argument("--scale", default=1, type=float, help="downsize the image by scale, must be <=1")
+    parser.add_argument("--hiera", default=0, type=int, help="hierarchical inference (only needed for high-resolution images (>1K))")
+    parser.add_argument("--z_far", default=10, type=float, help="max depth to clip in point cloud")
+    parser.add_argument("--valid_iters", type=int, default=32, help="number of flow-field updates during forward pass")
+    parser.add_argument("--get_pc", type=int, default=1, help="save point cloud output")
+    parser.add_argument(
+        "--remove_invisible",
+        default=1,
+        type=int,
+        help="remove non-overlapping observations between left and right images from point cloud, so the remaining points are more reliable",
+    )
+    parser.add_argument("--denoise_cloud", type=int, default=1, help="whether to denoise the point cloud")
+    parser.add_argument("--denoise_nb_points", type=int, default=30, help="number of points to consider for radius outlier removal")
+    parser.add_argument("--denoise_radius", type=float, default=0.03, help="radius to use for outlier removal")
+    args = parser.parse_args()
+
+    set_logging_format()
+    set_seed(0)
+    torch.autograd.set_grad_enabled(False)
+    os.makedirs(args.out_dir, exist_ok=True)
+
+    ckpt_dir = args.ckpt_dir
+    cfg = OmegaConf.load(f"{os.path.dirname(ckpt_dir)}/cfg.yaml")
+    if "vit_size" not in cfg:
+        cfg["vit_size"] = "vitl"
+    for k in args.__dict__:
+        cfg[k] = args.__dict__[k]
+    args = OmegaConf.create(cfg)
+    logging.info(f"args:\n{args}")
+    logging.info(f"Using pretrained model from {ckpt_dir}")
+
+    model = FoundationStereo(args)
+
+    ckpt = torch.load(ckpt_dir)
+    logging.info(f"ckpt global_step:{ckpt['global_step']}, epoch:{ckpt['epoch']}")
+    model.load_state_dict(ckpt["model"])
+
+    model.cuda()
+    model.eval()
+    cap = cv2.VideoCapture(args.video)
+    writter = cv2.VideoWriter("./vis.png", cv2.VideoWriter_fourcc(*"mjpg"), 30, (1920, 1080))
+    scale = args.scale
+    assert scale <= 1, "scale must be <=1"
+    H, W = (1920, 1080)
+    index = 0
+    while True:
+        ret, img = cap.read()
+        if not ret:
+            break
+        index += 1
+
+        img0 = img[:, :1920]
+        img1 = img[:, 1920:]
+        img0_ori = img0.copy()
+        img0 = torch.as_tensor(img0).cuda().float()[None].permute(0, 3, 1, 2)
+        img1 = torch.as_tensor(img1).cuda().float()[None].permute(0, 3, 1, 2)
+        padder = InputPadder(img0.shape, divis_by=32, force_square=False)
+        img0, img1 = padder.pad(img0, img1)
+
+        with torch.cuda.amp.autocast(True):
+            if not args.hiera:
+                disp = model.forward(img0, img1, iters=args.valid_iters, test_mode=True)
+            else:
+                disp = model.run_hierachical(img0, img1, iters=args.valid_iters, test_mode=True, small_ratio=0.5)
+        disp = padder.unpad(disp.float())
+        disp = disp.data.cpu().numpy().reshape(H, W)
+        vis = vis_disparity(disp)
+        vis = np.concatenate([img0_ori, vis], axis=1)
+        writter.write(vis)
+    print(f"Finished, done : {index} frames")
